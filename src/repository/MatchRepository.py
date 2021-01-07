fbacdeecb0b3c758bfef08aa05e28218d3d4ba2b
Romuald Shmidtelson <d-w-b@mail.ru>
2021-01-07 15:46:52 +0300

    Updated match repository

--- src/repository/MatchRepository.py
+++ src/repository/MatchRepository.py
@@ -1,7 +1,9 @@
+from sqlalchemy import desc
+
 from src.abstract.DBAbstract import DBAbstract
 from src.models import MatchModel
 
 
 class MatchRepository(DBAbstract):
-    def get_all(self, limit: int):
-        return self.db.query(MatchModel).all()
+    def get_all(self, limit: int = 10):
+        return self.db.query(MatchModel).order_by(desc(MatchModel.created_at)).limit(limit).all()
