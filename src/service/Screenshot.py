8a87edb4084809ee27d68f34a5444eaa5581032d
Romuald Shmidtelson <d-w-b@mail.ru>
2021-01-10 12:01:37 +0300

    Updated Screenshot.py

--- src/service/Screenshot.py
+++ src/service/Screenshot.py
@@ -19,16 +19,16 @@ class Screenshot(LoggerAbstract):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
 
-        while True:
-            try:
-                with webdriver.Remote(
-                    command_executor=f'{self.config.get_url_selenium()}/wd/hub',
-                    desired_capabilities=DesiredCapabilities.CHROME,
-                    options=options
-                ) as browser:
-                    browser.set_script_timeout(30)
-                    browser.set_page_load_timeout(30)
-                    browser.get(link)
-                return browser.get_screenshot_as_png()
-            except Exception as e:
-                self.logger.error(f'{e}, link:{link}', exc_info=True)
+        try:
+            browser = webdriver.Remote(
+                command_executor=f'{self.config.get_url_selenium()}/wd/hub',
+                desired_capabilities=DesiredCapabilities.CHROME,
+                options=options
+            )
+            browser.get(link)
+            result = browser.get_screenshot_as_png()
+            browser.quit()
+
+            return result
+        except Exception as e:
+            self.logger.error(f'{e}, link:{link}', exc_info=True)
