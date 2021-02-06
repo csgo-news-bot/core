b9b28083bd37c831e9a5cbbc13579ba4ec760b67
Romuald Shmidtelson <d-w-b@mail.ru>
2021-02-06 13:59:18 +0300

    Added more informed logger

--- src/thread/PublisherThread.py
+++ src/thread/PublisherThread.py
@@ -1,16 +1,20 @@
 import threading
 import time
 
+from src.abstract.LoggerAbstract import LoggerAbstract
 from src.service.publisher.Publisher import Publisher
 
 
-class PublisherThread(threading.Thread):
+class PublisherThread(threading.Thread, LoggerAbstract):
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
         while True:
-            publisher = Publisher()
-            publisher.execute()
+            try:
+                publisher = Publisher()
+                publisher.execute()
+            except Exception as e:
+                self.logger.error(e, exc_info=True)
 
             time.sleep(1000)  # every 18 min
