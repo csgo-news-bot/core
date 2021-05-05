7aaa323861e92fb43e4fcfadf102194f888582aa
Romuald Shmidtelson <d-w-b@mail.ru>
2021-05-05 15:01:01 +0300

    Added get_all methods to repositories

--- src/repository/TeamRepository.py
+++ src/repository/TeamRepository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.abstract.DBAbstract import DBAbstract
 from src.models import TeamModel
 
@@ -5,3 +7,6 @@ from src.models import TeamModel
 class TeamRepository(DBAbstract):
     def get_by_title(self, title: str) -> TeamModel:
         return self.db.query(TeamModel).filter(TeamModel.title == title).first()
+
+    def get_all(self) -> List[TeamModel]:
+        return self.db.query(TeamModel).all()
